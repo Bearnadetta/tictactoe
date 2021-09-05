a6c324fc40865c7c0e1aa6e7c116b8e2eb6b935e
Bearnadetta <bearnadetta@gmail.com>
2021-09-05 09:25:28 -0700

    Created Gameboard module
    The Gameboard module currently returns the makeBoard function as well as
    the boardArray array. The boardArray displays as a 3 x 3 grid within the
    tictactoe-game div. Each square in the boardArray has a data position attribute
    from 0 to 8, in order to represent the position on the board.

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-(function() {
+const Gameboard = (function() {
     'use strict';
-    
-    return {
-
+    let display = document.getElementById('tictactoe-game');
+    let boardArray = [];
+    const makeBoard = function() {
+        display.textContent = '';
+        for (let i = 0; i < 9; i++) {
+            let square = document.createElement('div');
+            square.className = 'square';
+            square.setAttribute('data-position', i);
+            boardArray.push(square);
+            display.appendChild(square);
+        }
     }
+    return {
+        makeBoard: makeBoard,
+        boardArray: boardArray,
+    };
 })();
+
+
+
+Gameboard.makeBoard();
