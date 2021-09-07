9689b30376b726ca705fb421f33d338f8eb0d26a
Bearnadetta <bearnadetta@gmail.com>
2021-09-07 12:56:59 -0700

    Reworked Player factory function as well as gameBoard and gameLogic modules
    The gameLogic module now correctly alternates between players when a square
    is clicked, and the winCheck function checks the boardArray against the
    winCons to see if a winner has been found. Alerts if there is a draw,
    and alerts if a player has won.

--- app.js
+++ app.js
@@ -1,86 +1,3 @@
-/*
-const Gameboard = (function() {
-    let display = document.getElementById('tictactoe-game');
-    let boardArray = ['','','','','','','','',''];
-    const reset = function() {
-        Gameboard.boardArray = ['','','','','','','','',''];
-        boardArray = ['','','','','','','','',''];
-        let squares = display.querySelectorAll('.square')
-        for (let i = 0; i < squares.length; i++) {
-            let square = (squares[i]);
-            square.remove();
-        }
-        Gameboard.makeBoard();
-    }
-    const makeBoard = function() {
-        display.textContent = '';
-        for (let i = 0; i < boardArray.length; i++) {
-            let square = document.createElement('div');
-            square.className = 'square';
-            square.setAttribute('data-position', i);
-            square.addEventListener('click', players.takeTurn);
-            square.textContent = (boardArray[i]); 
-            display.appendChild(square);
-        }
-    }
-    return {makeBoard, boardArray, reset};
-})();
-
-const players = (function(){
-    const playerFactory = (playerName, gamePiece) => {
-        takeTurn = function(){
-            let attribute = this.getAttribute('data-position');
-            let symbol = currentPlayer.gamePiece;
-            if(Gameboard.boardArray[attribute] === ''){
-                Gameboard.boardArray[attribute] = symbol;
-                Gameboard.makeBoard();
-                GameLogic.checkWin();
-                togglePlayer();
-            }
-        }
-        return { playerName, gamePiece, takeTurn};
-    }
-    
-    const playerOne = playerFactory('Player 1', 'X');
-    const playerTwo = playerFactory('Player 2', 'O');
-    let currentPlayer = playerOne;
-    let otherPlayer = playerTwo;
-    const togglePlayer = function() {
-        let storedVal = currentPlayer;
-        currentPlayer = otherPlayer;
-        otherPlayer = storedVal;
-        return(currentPlayer, otherPlayer);
-    }
-    return {playerOne, playerTwo, takeTurn, togglePlayer, currentPlayer, otherPlayer}
-})();
-
-const GameLogic = (function() {
-    let arrayValues = Gameboard.boardArray;
-    const checkWin = function(){
-        if((arrayValues[0] === arrayValues[1] && arrayValues[1] === arrayValues[2] && arrayValues[1] !== '') 
-        || (arrayValues[0] === arrayValues[3] && arrayValues[3] === arrayValues[6] && arrayValues[3] !== '')
-        || (arrayValues[0] === arrayValues[4] && arrayValues[4] === arrayValues[8] && arrayValues[4] !== '')
-        || (arrayValues[1] === arrayValues[4] && arrayValues[4] === arrayValues[7] && arrayValues[4] !== '')
-        || (arrayValues[2] === arrayValues[5] && arrayValues[5] === arrayValues[8] && arrayValues[5] !== '')
-        || (arrayValues[3] === arrayValues[4] && arrayValues[4] === arrayValues[5] && arrayValues[4] !== '')
-        || (arrayValues[6] === arrayValues[4] && arrayValues[4] === arrayValues[2] && arrayValues[4] !== '')
-        || (arrayValues[6] === arrayValues[7] && arrayValues[7] === arrayValues[8] && arrayValues[7] !== '')){
-            let winner = players.currentPlayer.playerName;
-            alert('The winner is ' + winner + '!');
-        } else if( arrayValues[0] !== '' && arrayValues[1] !== '' && arrayValues[2] !== '' && arrayValues[3] !== '' && arrayValues[4] !== ''
-        && arrayValues[5] !== '' && arrayValues[6] !== '' && arrayValues[7] !== '' && arrayValues[8] !== '') {
-            alert('it\'s a draw!');
-        }
-    }
-    return{checkWin,}
-})();
-
-Gameboard.makeBoard();
-//console.log(players);
-*/
-
-//let's try this, but better and with my failures under my belt
-
 const Player = (playerName, gamePiece) => {
     return {playerName, gamePiece};
 }
@@ -130,11 +47,23 @@ const gameLogic = (() => {
     function winCheck() {
         let myArray = gameBoard.boardArray
         console.log(myArray)
-        winCons.forEach((item, index) => {
+        let winner = ''
+        let gameOver = false
+        winCons.forEach((item) => {
             if (myArray[item[0]] === this.activePlayer.gamePiece && myArray[item[1]] === this.activePlayer.gamePiece && myArray[item[2]] === this.activePlayer.gamePiece) {
-                return console.log('Game Over!')
-            }
+                winner = this.activePlayer.playerName
+            } 
         })
+        if (!myArray.includes('') && winner === '') {
+            gameOver = true;
+            return alert('it\'s a draw!')
+        } else if (winner !== '') {
+            gameOver = true;
+            return alert('Game Over! ' + winner + ' is the winner!') 
+        }
+        if(gameOver === true) {
+
+        }
     }
 
     return {activePlayer, togglePlayer, winCheck};
