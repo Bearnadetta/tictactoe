42e9ec0c346da5828cb44da282e2b704a12ecf32
Bearnadetta <bearnadetta@gmail.com>
2021-09-15 08:49:16 -0700

    Added winCheck function to game module, currently not working
    The reset function now properly clears the board of all game pieces and
    resets the gameState, grid, and active player. winCheck function is not
    accurately determining a winner, but is capable of recognizing when the
    grid is full.

--- app.js
+++ app.js
@@ -29,12 +29,34 @@ const game = (() => {
     const winCons = [[0,1,2,],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]; 
     //starts and resets the game by emptying squares
     function reset() {
-        squares.forEach(square => square.textContent = '')
-        this.gameState =gameBoard.boardArray;
+        grid.forEach(square => square.textContent = '')
+        gameState = ['','','','','','','','',''];
+        activePlayer = playerOne;
     }
     //switches the current active player
     function togglePlayer() {
         activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
+    }
+    function winCheck() {
+        let winner = ''
+        let gameOver = false
+        console.log(activePlayer.gamePiece)
+        winCons.forEach((item) => {
+            if (gameState[item[0]] === activePlayer.gamePiece && gameState[item[1]] === gameState.gamePiece && gameState[item[2]] === activePlayer.gamePiece) {
+                winner = activePlayer.playerName
+                
+            } 
+        })
+        if (!gameState.includes('') && winner === '') {
+            gameOver = true;
+            return alert('it\'s a draw!')
+        } else if (winner !== '') {
+            gameOver = true;
+            return alert('Game Over! ' + winner + ' is the winner!') 
+        }
+        if(gameOver === true) {
+
+        }
     }
     function boardSet() {
         grid.forEach((square, i) => {
@@ -42,13 +64,19 @@ const game = (() => {
             square.addEventListener('click', () => {
             let data = square.getAttribute('data-position')
             console.log(data);
-            if (gameBoard.boardArray[data] === '') {
-                gameBoard.boardArray[data] = activePlayer.gamePiece
+            if (gameState[data] === '') {
+                gameState[data] = activePlayer.gamePiece
                 square.textContent = activePlayer.gamePiece
+                winCheck();
                 togglePlayer();
+                
             }
             })
         }) 
+        resetBtn.addEventListener('click', () => {
+            reset();
+            console.log(gameState)
+        })
     }
     
     return{gameState, reset, togglePlayer, boardSet}
