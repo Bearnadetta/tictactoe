e395b8138b97bb1c18f24bb1056db8a59a59bffe
Bearnadetta <bearnadetta@gmail.com>
2021-09-19 09:05:36 -0700

    buttons now working as intended, game logic working as intended, CSS stylings needed

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ const game = (() => {
     let playerOne = Player(playerX, 'X')
     let playerTwo = Player(playerO, 'O')
     let activePlayer = playerOne; 
+    let boardIsSet = false;
     //an Array of potential win conditions
     const winCons = [[0,1,2,],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]; 
     //starts and resets the game by emptying squares
@@ -100,7 +101,9 @@ const game = (() => {
     }
     // new game button to bring up menu and allow for renaming
     function newGame() {
-
+        reset()
+        displayController.gameMenu.classList.remove('invisible')
+        winMessage.textContent = 'Begin with these players?'
     }
     //switches the current active player
     function togglePlayer() {
@@ -115,32 +118,39 @@ const game = (() => {
         })
         if (!gameState.includes('') && winner === '') {
             gameOver = true;
-            return alert('it\'s a draw!')
+            displayController.gameMenu.classList.remove('invisible')
+            winMessage.textContent = 'It\'s a draw! Play again?'
+            reset()
         } else if (winner !== '') {
             gameOver = true;
             grid.forEach(square => square.classList.remove('empty'));
-            return alert('Game Over! ' + winner + ' is the winner!') 
+            displayController.gameMenu.classList.remove('invisible')
+            winMessage.textContent = (winner + ' is the winner! Play again?')
+            reset()
         }
     }
     function boardSet() {
-        grid.forEach((square, i) => {
-            square.setAttribute('data-position', i)
-            square.classList.add('empty')
-            let data = square.getAttribute('data-position')
-            square.addEventListener('click', () => {
-            if (gameState[data] === '' && gameOver === false) {
-                gameState[data] = activePlayer.gamePiece
-                square.textContent = activePlayer.gamePiece
-                square.classList.remove('empty')
-                winCheck();
-                togglePlayer();
-            }
-            })    
-        }) 
-        resetBtn.addEventListener('click', () => {
-            reset();
-            console.log(gameState)
-        })
+        if(boardIsSet === false) {
+            grid.forEach((square, i) => {
+                square.setAttribute('data-position', i)
+                square.classList.add('empty')
+                let data = square.getAttribute('data-position')
+                square.addEventListener('click', () => {
+                if (gameState[data] === '' && gameOver === false) {
+                    gameState[data] = activePlayer.gamePiece
+                    square.textContent = activePlayer.gamePiece
+                    square.classList.remove('empty')
+                    winCheck();
+                    togglePlayer();
+                }
+                })    
+            }) 
+            resetBtn.addEventListener('click', () => {
+                reset();
+                console.log(gameState)
+            })
+        }
+        boardIsSet = true;
     }
     
     return{gameState, reset, togglePlayer, boardSet, newGame, startGame}
